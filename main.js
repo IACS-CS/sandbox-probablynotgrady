06f4564e3ce3fb1cbe76c97ccdfd865e9ea0e607
probablynotgrady <ipodidiot1000@gmail.com>
2025-09-03 16:25:58 +0000

    Messed up the promtpt cotntrol

--- main.js
+++ main.js
@@ -8,5 +8,9 @@ let ti = new TextInterface(app, "Example Text Interface Program");
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
-
-
+let canhelp = ti.promptYesOrNo("Can I help you with anything?");
+if (canhelp) {
+  ti.output("thats great, now go away");
+} else {
+  ti.output("Oh wonderful, please leave now");
+  }
