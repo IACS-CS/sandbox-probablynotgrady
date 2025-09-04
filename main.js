c781bbe666510e105917c837a063ae89427f7cf6
probablynotgrady <ipodidiot1000@gmail.com>
2025-09-04 18:41:33 +0000

    changed the type of program, insults user if they try to legitimately use the question.

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface Program");
+let ti = new TextInterface(app, "MS-DOS Helper Program");
 
-ti.output("Welcome to my program!");
+ti.output("Welcome to the MS-DOS Helper Program!");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
-let canhelp = ti.promptYesOrNo("Can I help you with anything?");
-if (canhelp) {
-  ti.output("thats great, now go away");
-} else {
-  ti.output("Oh wonderful, please leave now");
+
+//ask if can help, if yes then say "great go away" if no then say "oh wonderful please leave now"
+  let help = await ti.prompt("How can I help");
+  if (help==="DOS", "dos", "Dos") {
+    ti.output("DOS is too old, get a new computer");
+    ti.output("Go use something modern you ancient dipshit");
+    ti.output("get outta here");
+  } else {
+    ti.output("I cant help you. Maybe try Google?");
   }
