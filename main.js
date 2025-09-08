fc64ef627a362c6e631bfba8515af57ba2245cbb
probablynotgrady <ipodidiot1000@gmail.com>
2025-09-08 12:52:05 +0000

    idk what i did

--- main.js
+++ main.js
@@ -6,34 +6,34 @@ let app = document.querySelector("#app");
 let ti = new TextInterface(app, "MS-DOS Helper Program");
 
 ti.output("Welcome to the MS-DOS Helper Program!");
-let name = await ti.prompt("What is your name?");
-ti.output("Hello, " + name);
 
-//ask if can help, if yes then say "great go away" if no then say "oh wonderful please leave now"
-  let repeatNoRude = true;
-while (repeatNoRude) {
+//ask if can help, if user says DOS, say "go use something modern like Windows NT or Linux, or god forbid MacOS"
+//if user says anything else, say "your message must include the word DOS"
+//if user says anything with "fuck" say "rude" and ask for apology. if no, then quit program.
+  let repeatAndAnnoyUser = true;
+  let answerRude = false;
+while (repeatAndAnnoyUser) {
   let help = await ti.prompt("How can I help?");
   if (help==="DOS" || help==="dos" || help==="Dos") {
     ti.output("Go use something modern like Windows NT or Linux, or god forbid MacOS");
     ti.output("get outta here!");
-    ti.output("(It is now safe to turn off your computer)");
-    repeatNoRude = false;
+    ti.output("(It is now safe to turn off your computer)")
+    repeatAndAnnoyUser = false;
   } else {
-    let repeatRude = true;
-    if (help.toLowerCase()==="fuck") {
-      ti.output("Hey, that's not very nice!");
-      ti.output("I refuse to help you until you apologize.");
-      let apology = await ti.prompt("Do you apologize?");
-      if (apology.toLowerCase()==="yes") {
-        ti.output("Thank you. I appreciate your apology.");
-      } else {
-        ti.output("Fine, be that way. You're a dick.");
-        repeatRude = true;
-      }
+    ti.output("Your message must include the word DOS");   
     }
-    ti.output("Your message must include the word DOS");    
+    }
+if (help.toLowerCase().includes("fuck")) {
+  let repeatAndAnnoyUser = false;
+  answerRude = true;
+  if (answerRude) {
+  ti.output("Rude! I'm just trying to help!");
+  ti.output("I expect an apology from you right this instant!");
+  let apology = await ti.prompt("Do you apologize?");
+  if (apology.toLowerCase().includes("yes")) {
+    answerRude = false;
+    ti.output("Thank you for your apology.");
+    repeatAndAnnoyUser = true;
   }
-}
-if (repeatRude===true) {
-  ti.output("Go away, I refuse to help you.");
-}
+  }
+  }
