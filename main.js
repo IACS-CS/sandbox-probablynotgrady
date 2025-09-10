2be492eccc38e4a5f6f6b072d2ba58b7891782fe
probablynotgrady <ipodidiot1000@gmail.com>
2025-09-10 16:18:59 +0000

    got back to square 1 yayyy

--- main.js
+++ main.js
@@ -10,29 +10,30 @@ ti.output("Welcome to the MS-DOS Helper Program!");
 //ask if can help, if user says DOS, say "go use something modern like Windows NT or Linux, or god forbid MacOS"
 //if user says anything else, say "your message must include the word DOS"
 //if user says anything with "fuck" say "rude" and ask for apology. if no, then quit program.
-  let repeatAndAnnoyUser = true;
-  let answerRude = false;
-while (repeatAndAnnoyUser) {
+let repeatAndAnnoyUser = true;
+let exitRightAway = false;
+while (repeatAndAnnoyUser && !exitRightAway) {
   let help = await ti.prompt("How can I help?");
-  if (help==="DOS" || help==="dos" || help==="Dos") {
-    ti.output("Go use something modern like Windows NT or Linux, or god forbid MacOS");
-    ti.output("(It is now safe to turn off your computer)")
+  if (help === "DOS" || help === "dos" || help === "Dos") {
+    ti.output(
+      "Go use something modern like Windows NT or Linux, or god forbid MacOS"
+    );
+    ti.output("(It is now safe to turn off your computer)");
     repeatAndAnnoyUser = false;
-  if (help==="swear") {
-      repeatAndAnnoyUser = false;
-      answerRude = true;
-      if (answerRude) {
-        let rude = await ti.prompt("Rude. Apologize.");
-        if (rude.toLowerCase()==="sorry") {
-          answerRude = false;
-          ti.output ("Apology accepted.");
-          } else {
-          ti.output("I refuse to help you.");
-        }
+  } else {
+    if (help.toLowerCase()==="swear") {
+      let apology = await ti.prompt("Rude. Apologize.");
+      if (apology.toLowerCase()==="sorry") {
+        ti.output ("Apology accepted.");
+      } else {
+        exitRightAway = true;
+        ti.output("Goodbye. I don't like you.");
       }
     }
-}else {
-  ti.output ("Your message must include the word DOS");
+    if (!exitRightAway) {
+      ti.output("Your message must include the word DOS");
+    }
+  }
 }
-} 
+
 // idk what the hell i did but not it dont work
